b23f7d5d7073645ca9603984a79026b695c9e989
Niki Simple <shpakovskiynl@gmail.com>
2018-09-28 02:29:33 +0300

    first page done

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,6 +1,6 @@
 //$(document).ready(function() {
 
-$(function() {
+  $(function() {
 
     svg4everybody();
     objectFitImages();
@@ -10,376 +10,52 @@ $(function() {
     //dynamicHeight($('.you_class'));
 
 
-    $('.main-action__btn').hover(function() {
-        $('.top-block').addClass('top-block--action');
+    $('.main-action__box--js').hover(function() {
+      $('.top-block').addClass('top-block--action');
     }, function() {
-        $('.top-block').removeClass('top-block--action');
+      $('.top-block').removeClass('top-block--action');
     });
 
+
+
+    {
+      var toolTips = $('.gift__desc');
+      toolTips.each(function(index, el) {
+        var toolData = $(this).data('tooltips');
+        new Tooltip(el, {
+          title: toolData,
+          placement: 'bottom',
+          html: true,
+              // trigger: 'click'
+            });
+      });
+    }//tooltips
+
+    {
+        $('.gift-chosen').hide();
+
+      $('.gift__action .btn').on('click', function(event) {
+        var curTitle = $(this).parents('.gift-part').find('.gift__title').text();
+        $('.gift-chosen b').text(curTitle);
+        $('.gift-chosen').slideDown();
+        // console.log("curTitle", curTitle);
+        $(this).text('Выбрано').parents('.gift-part').addClass('gift-part--current').siblings().removeClass('gift-part--current').find('.btn').text('Выбрать');
+      });
+
+    }//gift block actions
 
 
     var $form = $('.form');
 
     $form.on('click', function(event) {
-        var formId = $(this).attr("id");
+      var formId = $(this).attr("id");
         // console.log('formId ' + formId); 
         localStorage.setItem('form', formId);
         var pageName = window.location.pathname;
         // console.log('pageName ' + pageName);
         localStorage.setItem('currentPage', pageName);
-    });
+      });
 
-    //	E-mail Ajax Send
-    $form.each(function() {
-        //    var $this = $(this);
-        $(this).validate({
-
-            // rules: {
-            // 	phone: {
-            // 		required: true,
-            // 		minlength: 6,
-            // 		number: true
-            // 	}
-            // }, 
-
-            submitHandler: function(form) {
-                var formData = new FormData(form);
-                $.ajax({
-                    type: "POST",
-                    url: "mail.php",
-                    data: formData,
-                    contentType: false,
-                    dataType: "json",
-                    processData: false,
-                    beforeSend: function() {
-                        $(form).find('.btn').attr("disabled", true);
-                        $(form).find('.form-load').css({
-                            'width': '20px',
-                            'margin-left': '10px'
-                        });
-                        console.log('before send')
-                    }
-                }).done(function() {
-                    $(form).find('.btn').attr("disabled", false);
-                    $(form).find('.form-load').css({
-                        'width': '0',
-                        'margin-left': '0'
-                    });
-                    $(form).trigger("reset");
-                    $.magnificPopup.close();
-                    //window.location.href = "thanks.html";
-                    console.log('done')
-                }).fail(function() {
-                    alert("Error, email not sent !");
-                    console.log('error')
-                });
-            }
-        });
-    });
-
-    //popup form
-    $('.popup-js').magnificPopup({
-        type: 'inline',
-        preloader: false,
-        focus: '#name',
-        callbacks: {
-            beforeOpen: function() {
-                if ($(window).width() < 700) {
-                    this.st.focus = false;
-                } else {
-                    this.st.focus = '#name';
-                }
-            }
-        }
-    });
-
-    //phone mask
-    // $(".phone").mask("+9(999)999-99-99");
-
-    //animate pege element
-    //	 if (screen.width > 768) {
-    //	   $(".mission h2").animated("bounceIn");
-    //		 $(".mission h2").css('animation-delay', '0.5s').animated("bounceInLeft");
-    //	 };
-
-    //form styler
-    // $('input, select').styler();
-
-
-    //menu on click
-    // var startWidth = 1100;
-    // var menu = $('.header__list');
-    // var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-    // $(window).resize(function(){
-    // windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    //   if(windowWidth > startWidth && menu.is(':hidden')) {
-    //     menu.attr("style", "");
-    //   };
-    // });
-
-    // $('#openup').on('click', function(e) {
-    //   e.preventDefault();
-    //   menu.stop(true,true).slideToggle();
-    //   $('.hamburger--collapse').stop(true,true).toggleClass('is-active');
-    //   $('.header__list-sublist-wrap').css('display', 'none');
-    // });
-
-
-    // $('.header__list-item').on('click', '.header__list-link', function(e) {
-    //   $this = $(e.delegateTarget);
-
-    //   if($this.find('.header__list-sublist-wrap').length>0){
-    //     e.preventDefault();
-    //     $this.children('.header__list-sublist-wrap').stop(true,true).slideToggle(10).parent('.header__list-item').siblings().children('.header__list-sublist-wrap').css('display', 'none');
-
-    //   }else{
-    //     $('.header__list-sublist-wrap').css('display', 'none');
-    //     if(windowWidth < startWidth){
-    //       menu.stop(true,true).slideToggle();
-    //       $('.hamburger--collapse').stop(true,true).toggleClass('is-active');
-    //     };
-    //   };
-    // });
-
-    // $('.header-sublist__link').on('click',  function(event) {
-    //   $('.header__list-sublist-wrap').css('display', 'none');
-    //   if(windowWidth < startWidth){
-    //     menu.stop(true,true).slideToggle();
-    //     $('.hamburger--collapse').stop(true,true).toggleClass('is-active');
-    //   };
-    // });
-    // // close menu if click on "body"
-    // $(document).mouseup(function (e) {
-    //   if(menu.is(":visible") && $(e.target).closest('.header__nav-wrap').length == 0 && windowWidth < startWidth){
-    //     $('.header__list-sublist-wrap').css('display', 'none');
-    //     menu.stop(true,true).slideToggle();
-    //     $('.hamburger--collapse').stop(true,true).toggleClass('is-active');
-    //     console.log('close menu if click on "body"')
-    //   }
-    // });
-
-    // menu on click end
-
-
-    //menu on hover
-    var startWidth = 1100;
-    var menu = $('.header__list');
-    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-    function removeInline() {
-        if (windowWidth > startWidth) {
-            menu.attr("style", "");
-
-            $('.header__list-sublist-wrap').attr("style", "");
-            $('.hamburger--collapse').removeClass('is-active');
-        };
-    };
-    // removeInline();
-
-    function menuOn() {
-        //activate the menu only if the width of the window is less than the specified
-        if (windowWidth <= startWidth) {
-
-            //open menu
-            $('#openup').on('click', function(e) {
-                e.preventDefault();
-                $('.header__list-open-submenu').removeClass('header__list-open-submenu--open');
-                $('.hamburger--collapse').stop(true, true).toggleClass('is-active');
-                $('.header__list-sublist-wrap').attr("style", "");
-                menu.stop(true, true).slideToggle();
-            });
-
-            //hide the menu when clicking on the first and second level link
-            $('.header__list-link,.header-sublist__link').on('click', function(event) {
-                $('.header__list-sublist-wrap').css('display', 'none');
-                $('.header__list-open-submenu').removeClass('header__list-open-submenu--open');
-                menu.stop(true, true).slideToggle();
-                $('.hamburger--collapse').stop(true, true).toggleClass('is-active');
-            });
-
-            //open submenu
-            $('.header__list-open-submenu').on('click', function(event) {
-                if ($(this).hasClass("header__list-open-submenu--open")) {
-                    $('.header__list-open-submenu').removeClass('header__list-open-submenu--open');
-                    $('.header__list-sublist-wrap').attr("style", "");
-                    $(this).removeClass('header__list-open-submenu--open').parents('.header__list-item').find('.header__list-sublist-wrap').attr("style", "");
-                    // console.log('true')
-                } else {
-                    $('.header__list-open-submenu').removeClass('header__list-open-submenu--open');
-                    $('.header__list-sublist-wrap').attr("style", "");
-                    $(this).addClass('header__list-open-submenu--open').parents('.header__list-item').find('.header__list-sublist-wrap').css('display', 'block');
-                    // console.log('false')
-                }
-            });
-
-            // close menu if click on "body"
-            $(document).mouseup(function(e) {
-                if (menu.is(":visible") && $(e.target).closest('.header__nav-wrap').length == 0) {
-                    $('.header__list-sublist-wrap').css('display', 'none');
-                    $('.header__list-open-submenu').removeClass('header__list-open-submenu--open');
-                    menu.stop(true, true).slideToggle();
-                    $('.hamburger--collapse').stop(true, true).toggleClass('is-active');
-                    // console.log('close menu if click on "body"')
-                }
-            });
-
-        };
-        /*if < window width*/
-
-    };
-    /*menuON*/
-    menuOn();
-
-    $(window).resize(function() {
-        windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        // console.log("windowWidth", windowWidth);
-        removeInline();
-        refreshVar(); //part of the function of fixing the menu
-    });
-    //menu on hover end
-
-    //menu scroll
-    //  $(".easyscroll").on("click", function (event) {
-    //    event.preventDefault();
-    //    var id  = $(this).attr('href'),
-    //    top = $(id).offset().top;
-    //    $('body,html').animate({scrollTop: top}, 600);
-    //  });
-
-    //menu fixed on scroll
-    var navPos, winPos, navHeight;
-
-    function refreshVar() {
-        // navPos = $('nav').offset().top;
-        navPos = $('.header').outerHeight(true) / 2;
-        // console.log("navPos", navPos);
-        navHeight = $('.header__top-line').outerHeight(true);
-        // console.log("navHeight", navHeight);
-    };
-    refreshVar();
-
-    $('<div class="header__top-line--clone"></div>').insertBefore('.header__top-line').css('height', navHeight).hide();
-
-    $(window).scroll(function() {
-
-        winPos = $(window).scrollTop();
-        // console.log("winPos", winPos);
-        if (winPos >= navPos) {
-            $('.header__top-line').addClass('header__top-line--fixed');
-            $('.header__top-line--clone').show();
-        } else {
-            $('.header__top-line').removeClass('header__top-line--fixed');
-            $('.header__top-line--clone').hide();
-        }
-
-    });
-    //menu fixed on scroll end
-
-    //menu scroll and add class on scroll
-
-    //scroll
-    var navHeight = $(".header__top-line").outerHeight(true);
-
-    function showSection(section, isAnimate) {
-        var direction = section.replace(/#/, '');
-        // console.log(direction)
-        var reqSection = $('.section').filter('[data-section="' + direction + '"]');
-
-        var reqSectionPos = reqSection.offset().top - navHeight + 1;
-        // console.log("reqSectionPos", reqSectionPos);
-
-        if (isAnimate) {
-            $('body, html').animate({
-                scrollTop: reqSectionPos
-            }, 500);
-        } else {
-            $('body, html').scrollTop(reqSectionPos);
-        }
-    };
-
-    //вызывать по клику  showSection($(this).attr('href'), true);
-    // $('.header__list-link').on('click', function (event) {
-    //   event.preventDefault();
-    //   showSection($(this).attr('href'), true);
-    // });
-    //scroll end
-
-    //add class
-    function checkSection() {
-
-        $('.section').each(function() {
-            var $this = $(this),
-                topEdge = $this.offset().top - $('.header__top-line').outerHeight(true),
-                bottomEdge = topEdge + $this.outerHeight(true),
-                wScroll = $(window).scrollTop();
-
-            if (topEdge < wScroll && bottomEdge > wScroll) {
-
-                var currentId = $this.data('section'),
-                    reqLink = $('.header__list-link').filter('[href="#' + currentId + '"]');
-
-                reqLink.closest('.header__list-item').addClass('header__list-item--active').siblings().removeClass('header__list-item--active');
-
-                //простовляния хешей в адрессную строку
-                if (history.pushState) {
-                    history.pushState(null, null, "#" + currentId);
-                    // console.log("modern work");
-                } else {
-                    // window.location.hash = currentId;
-                    location.hash = currentId;
-                    // console.log("old work");
-                }
-
-            }
-        });
-
-    };
-
-    // уменьшаю количество вызова функций при скролле
-    /*
-    var scrollTimeout;  // global for any pending scrollTimeout
-    $(window).scroll(function () {
-      if (scrollTimeout) {
-        // clear the timeout, if one is pending
-        clearTimeout(scrollTimeout);
-        scrollTimeout = null;
-      }
-      scrollTimeout = setTimeout(checkSection, 500);
-    });
-    */
-
-    var didScroll = false;
-
-    window.onscroll = doThisStuffOnScroll;
-
-    function doThisStuffOnScroll() {
-        didScroll = true;
-    }
-
-    setInterval(function() {
-        if (didScroll) {
-            didScroll = false;
-            checkSection();
-        }
-    }, 500);
-
-    //add class end
-
-    //menu scroll and add class on scroll end
-
-    // timer
-    // in html: .counter#clock
-    // var finishData = moment.tz("2018-09-12 13:00", "Europe/Moscow"),
-    //     countTextDay = 'Day',
-    //     countTextHour = 'Hour',
-    //     countTextMinute = 'Minute',
-    //     countTextSecond = 'Second';
-
-    // $('#clock').countdown(finishData.toDate(), function(event) {
-    //     // $(this).html(event.strftime('<span>%D</span>  <span>%H</span> <span>%M</span> <span> %S</span>'));
-    //     $(this).html(event.strftime('<div class="counter__item counter__item--day"><div class="counter__item-info"><div class="counter__item-numb">%D</div><div class="counter__item-title">' + countTextDay + '</div></div><div class="counter__item-dots"><svg xmlns="http://www.w3.org/2000/svg" width="202.857" height="570" viewBox="0 0 53.673 150.813"><g transform="translate(-27.403 -29.582)"><circle cx="54.618" cy="56.04" r="26.458"/><circle cx="53.862" cy="153.936" r="26.458"/></g></svg></div></div><div class="counter__item counter__item--hour"><div class="counter__item-info"><div class="counter__item-numb">%H</div><div class="counter__item-title">' + countTextHour + '</div></div><div class="counter__item-dots"><svg xmlns="http://www.w3.org/2000/svg" width="202.857" height="570" viewBox="0 0 53.673 150.813"><g transform="translate(-27.403 -29.582)"><circle cx="54.618" cy="56.04" r="26.458"/><circle cx="53.862" cy="153.936" r="26.458"/></g></svg></div></div><div class="counter__item counter__item--minute"><div class="counter__item-info"><div class="counter__item-numb">%M</div><div class="counter__item-title">' + countTextMinute + '</div></div><div class="counter__item-dots"><svg xmlns="http://www.w3.org/2000/svg" width="202.857" height="570" viewBox="0 0 53.673 150.813"><g transform="translate(-27.403 -29.582)"><circle cx="54.618" cy="56.04" r="26.458"/><circle cx="53.862" cy="153.936" r="26.458"/></g></svg></div></div><div class="counter__item counter__item--second"><div class="counter__item-info"><div class="counter__item-numb">%S</div><div class="counter__item-title">' + countTextSecond + '</div></div></div>'));
-    // });
 
 
 
@@ -388,19 +64,19 @@ $(function() {
 
 //removeIf(production)
 function pageWidget(pages) {
-    var widgetWrap = $('<div class="widget_wrap"><ul  class="widget_list"></ul></div>');
-    widgetWrap.prependTo("body");
-    var widgetPageList = '';
-    for (var i = 0; i < pages.length; i++) {
-        $('<li class="widget_item"><a class="widget_link" href="' + pages[i] + '.html' + '">' + pages[i] + '</a></li>').appendTo('.widget_list')
-        widgetPageList += '<div>http://webgenesis.pw/project_name/' + pages[i] + '.html</div>';
+  var widgetWrap = $('<div class="widget_wrap"><ul  class="widget_list"></ul></div>');
+  widgetWrap.prependTo("body");
+  var widgetPageList = '';
+  for (var i = 0; i < pages.length; i++) {
+    $('<li class="widget_item"><a class="widget_link" href="' + pages[i] + '.html' + '">' + pages[i] + '</a></li>').appendTo('.widget_list')
+    widgetPageList += '<div>http://webgenesis.pw/project_name/' + pages[i] + '.html</div>';
         //$("body").append("<div>http://webgenesis.pw/project_name/"+pages[i]+".html</div>"); //добавляет вниз бади все адреса 
-    }
+      }
     // $("body").html(widgetPageList); //меняет весь контент в бади
     var widgetStilization = $('<style>body{position:relative}.widget_wrap{position:fixed;top:0;left:-23px;z-index:9999;padding:10px 20px;background:#222;border-bottom-right-radius:10px;transition:all .3s ease;transform:translate(-100%,0)}.widget_wrap ul{max-width:220px;width:100%;display:flex;flex-wrap:wrap}.widget_wrap:after{content:" ";position:absolute;top:0;left:100%;width:24px;height:24px;background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAAAxQTFRF////////AAAA////BQBkwgAAAAN0Uk5TxMMAjAd+zwAAACNJREFUCNdjqP///y/DfyBg+LVq1Xoo8W8/CkFYAmwA0Kg/AFcANT5fe7l4AAAAAElFTkSuQmCC) 50% 50% no-repeat #222;cursor:pointer}.widget_wrap:hover{left:0;transform:translate(0,0)}.widget_item{padding:0 0 10px}.widget_link{display:block;color:#fff;text-decoration:none;font-size:15px;width:100px}.widget_link:hover{color:#fff;text-decoration:underline}</style>');
     widgetStilization.prependTo(".widget_wrap")
-};
-pageWidget(['index', 'thanks'])
+  };
+  pageWidget(['index', 'thanks'])
 
 
 //pixel-glass-js-master
